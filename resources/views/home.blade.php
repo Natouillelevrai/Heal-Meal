173f5d9dc28de7918d80ef26ef2953b9c3c86a2f
a442147 <a442147@edenschool.fr>
2025-06-17 13:36:52 +0200

    Fixed font weight

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout :title="$title">
     <div class="flex flex-col items-center inter">
-        <h1 class="text-3xl font-light">Welcome</h1>
-        <a href="#" class="relative inline-block text-xl font-light group">
+        <h1 class="text-3xl">Welcome</h1>
+        <a href="#" class="relative inline-block text-xl group">
             This is Heal Meal 
             <i class="ri-arrow-right-line"></i>
             <span class="absolute left-0 -bottom-1 w-full h-[2px] bg-black scale-x-0 group-hover:scale-x-100 transition-transform duration-500 origin-left"></span>
