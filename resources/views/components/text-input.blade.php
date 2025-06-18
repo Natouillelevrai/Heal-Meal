188ae2aaa788ecb7bfe0b1697296261584572cdf
TryaNever <112401558+TryaNever@users.noreply.github.com>
2025-06-18 15:27:48 +0200

    Revert "Finish Front Login page | Desktop Responsive TODO"

--- resources/views/components/text-input.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['disabled' => false])
-
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'text-[#6E7173] bg-white px-4 py-2 ring-0 focus:ring-1 focus:ring-[#B7E7EB] rounded-lg shadow-sm']) }}>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 <x-app-layout :title="$title">
+
     <div class="flex flex-col items-center justify-center inter h-screen">
+
         <h1 class="text-3xl">Welcome</h1>
         <a href="#" class="relative inline-block text-xl group">
             This is Heal Meal
@@ -7,4 +9,8 @@
             <span class="absolute left-0 -bottom-1 w-full h-[2px] bg-black scale-x-0 group-hover:scale-x-100 transition-transform duration-500 origin-left"></span>
         </a>
     </div>
+
+
+
 </x-app-layout>
+
