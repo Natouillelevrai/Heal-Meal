01c624f28a9e38c045695ed4db4e6cfe40a88feb
TryaNever <alexispernette@outlook.fr>
2025-06-18 10:13:29 +0200

    Add Structure html & ADD Script JS burger menu & ADD part of css

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+    <nav class="bg-[#B7E7EB] pl-3 px-2 w-full h-14 flex justify-around items-center rounded-xl ring-3 ring-white shadow-md shadow-black">
+        <div class="h-12">
+            <img class="h-full w-full" src="/images/logo.png" alt="">
+        </div>
+        <form action="" method="">
+            <div class="bg-white pl-5 rounded-lg w-38 flex">
+                <i class="ri-search-line text-[#6E7173]"></i>
+                <input class="pl-3 w-26 focus:outline-none" type="text" placeholder="Search">
+            </div>
+        </form>
+        <div class="h-7 w-7 bg-white rounded-md">
+
+        </div>
+    </nav>
+</header>
