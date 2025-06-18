e63197000a4e600e8bb024ed450b2a84ca700e5f
TryaNever <alexispernette@outlook.fr>
2025-06-18 11:53:39 +0200

    Patch fail

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,26 +12,6 @@
     </div>
 
 
-    <div class="carousel relative w-full h-48 max-w-xl overflow-hidden mx-auto">
-        <div class="h-full carousel-track flex transition-transform duration-500">
-            @foreach($recettes as $recette)
-                <img src="{{ $recette['image'] }}" alt="Recette" class="w-full h-full flex-shrink-0 object-cover" />
-            @endforeach
-        </div>
-
-        <button id="prev" class="absolute left-2 top-1/2 -translate-y-1/2 px-2 py-1 text-white text-5xl"><i
-                class="ri-arrow-left-s-line"></i></button>
-        <button id="next" class="absolute right-2 top-1/2 -translate-y-1/2 px-2 py-1 text-white text-5xl"><i
-                class="ri-arrow-right-s-line"></i></button>
-
-        <div class="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex gap-2 z-10">
-            @foreach($recettes as $recette)
-                <button class="dot w-3 h-3 rounded-full bg-white hover:bg-gray transition-all" data-slide="{{ $loop->index }}"></button>
-            @endforeach
-        </div>
-    </div>
-
-    @vite(['resources/js/slider.js'])
 
 </x-app-layout>
 
