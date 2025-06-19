7cca05b182ee5763296930e1166dff7a6f057ac5
dinopatates <gustavecheval819@gmail.com>
2025-06-19 15:40:52 +0200

    Recipes list CSS in progress

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use App\Models\Recette;
 
 class RecipesController
 {
 public function index() {
-        $recettes = Recette::paginate(12);
-        return view('recipes-catalog', ['recettes' => $recettes, 'title' => 'Recipes Catalog']);
+    $recettes = DB::table('recettes')
+        ->join('users', 'recettes.id_user', '=', 'users.id_user')
+        ->join('origins', 'recettes.id_origine', '=', 'origins.id_origine')
+        ->Join('fk_recettes_ingredients', 'recettes.id_recette', '=', 'fk_recettes_ingredients.id_recette')
+        ->leftJoin('ingredients', 'fk_recettes_ingredients.id_ingredient', '=', 'ingredients.id_ingredient')
+        ->leftJoin('fk_ingredient_regime', 'ingredients.id_ingredient', '=', 'fk_ingredient_regime.id_ingredient')
+        ->leftJoin('regime', 'fk_ingredient_regime.id_regime', '=', 'regime.id_regime')
+        ->select(
+            'recettes.*',
+            'users.username as user_name',
+            'origins.name as origin_name',
+            DB::raw('GROUP_CONCAT(DISTINCT ingredients.name) as ingredients'),
+            DB::raw('GROUP_CONCAT(DISTINCT regime.name) as regimes')
+        )
+        ->groupBy('recettes.id_recette')
+        ->paginate(12);
+
+    return view('recipes-catalog', [
+        'recettes' => $recettes,
+        'title' => 'Recipes Catalog'
+    ]);
     }
 }
--- resources/views/recipes-catalog.blade.php
+++ resources/views/recipes-catalog.blade.php
@@ -1,14 +1,23 @@
 <x-app-layout :title="$title">
 <section>
-    <div>
-        <div class="flex flex-col justify-around bg-color-white">
-           <h3>Nom</h3>
-            <p>Utilisateur</p>
-            <p>Catégories</p>
-        </div>
-        <div>
+    @foreach($recettes as $recette)
+    <div class="my-5 py-1 pl-4 flex flex-row justify-between bg-white h-30 rounded-xl shadow-sm  shadow-gray-500">
 
+        <div class="flex flex-col  justify-around h-full">
+           <h3 class="text-md font-semibold py-2">{{$recette->name}}</h3>
+            <p>{{$recette->user_name}}</p>
+            <div class="h-18 flex flex-col justify-around">
+            <p>{{$recette->origin_name}}</p>
+                <p class="text-sm">{{$recette->regimes}}</p>
+            </div>
+        </div>
+
+        <div class="w-7/12 h-full bg-amber-200 rounded-r-xl">
+            <div class="absolute h-28 w-14 bg-gradient bg-gradient-to-r from-white to-transparent">
+            </div>
+            <img class="w-full h-full rounded-r-2xl object-cover" src="/images/rice-fried-pork.png" alt="">
         </div>
     </div>
+        @endforeach
 </section>
 </x-app-layout>
