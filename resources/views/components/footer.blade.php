325398614a7bb012b1280eb6c50d62bc20f3b315
TryaNever <alexispernette@outlook.fr>
2025-06-19 08:58:01 +0200

    Finish Footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="bg-[#0E2F46] h-44 py-5 text-white text-2xl flex flex-col items-center justify-between">
+    <a href="http://127.0.0.1:8000/"><strong class="text-[#B7E7EB]">HealMeal</strong></a>
+    <ul class="flex w-1/2 justify-between">
+        <li>
+            <a href="https://facebook.com"><i class="ri-facebook-fill"></i></a>
+        </li>
+        <li>
+            <a href="https://instagram.com"><i class="ri-instagram-line"></i></a>
+        </li>
+        <li>
+            <a href="https://tiktok.com"><i class="ri-tiktok-fill"></i></a>
+        </li>
+    </ul>
+    <p>©2025 • HealMeal </p>
+</footer>
