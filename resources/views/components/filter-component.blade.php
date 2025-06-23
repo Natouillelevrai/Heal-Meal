41db12c8e413c6036e6b86f504c6f8f795759157
Drake <150128459+VialsShiny@users.noreply.github.com>
2025-06-23 16:54:21 +0200

    Finishing Filter Container

--- resources/views/components/filter-component.blade.php
+++ resources/views/components/filter-component.blade.php
@@ -1,6 +1,6 @@
 @props(['admin' => null])
 
-<div>
+<div class="w-full flex justify-between gap-x-12">
     @if (!$admin)
         <div class="bg-[#B7E7EB] flex rounded-xl">
             <x-filter-card name="teste" first="true" />
@@ -10,4 +10,7 @@
     @else
 
     @endif
+
+    <button role="option"
+        class="flex justify-center items-center capitalize max-h-12 min-w-12 px-4 py-2 bg-[#0E2F46] rounded-xl ring-3 ring-[#0E2F46] text-white">Filter</button>
 </div>
