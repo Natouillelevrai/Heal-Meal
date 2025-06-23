c030ff195ffa4fa1702e6547f2dcffb1d878495c
a442147 <a442147@edenschool.fr>
2025-06-23 10:19:34 +0200

    Fixed header search bar text

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,10 +4,10 @@
             <img class="h-full w-full rounded-full" src="{{ asset('images/logo.png') }}" alt="">
         </div>
         <form class="w-1/2">
-            <label class="bg-white pl-5 w-full rounded-lg flex" for="search">
+            <label class="bg-white px-3 w-full py-1 rounded-lg flex" for="search">
                 <i class="ri-search-line text-[#6E7173]"></i>
-                <input class="pl-3 w-full focus:outline-none" id="search" name="search" type="text"
-                       placeholder="Search">
+                <input class="pl-3 w-full focus:outline-none placeholder:text-sm" id="search" name="search" type="text"
+                       placeholder="Recherchez">
             </label>
         </form>
         <div class="h-8 w-8 bg-white rounded-md flex items-center justify-center button-burger">
