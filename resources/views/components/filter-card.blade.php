b45757e40272e75b6ade3eb6efc97814e3e90db5
Drake <150128459+VialsShiny@users.noreply.github.com>
2025-06-23 16:47:38 +0200

    Filter Style Not Finish

--- resources/views/components/filter-card.blade.php
+++ resources/views/components/filter-card.blade.php
@@ -3,5 +3,5 @@
 <div class="flex">
     <input type="radio" name="filter" id="{{ $name }}" class="hidden peer">
     <label for="{{ $name }}"
-        class="flex max-h-12 w-[150%] min-w-12 px-4 py-2 bg-white {{ !$last ? 'rounded-l-xl' : 'rounded-xl'}} border-3 peer-checked:bg-[#B7E7EB] peer-checked:text-white border-[#B7E7EB]">{{ $name }}</label>
+        class="flex capitalize max-h-12 w-[150%] min-w-12 px-4 py-2 bg-white {{ !$last ? 'rounded-l-xl' : 'rounded-xl'}} border-3 peer-checked:bg-[#B7E7EB] peer-checked:text-white border-[#B7E7EB] {{ !$first ? 'peer-checked:border-white' : '' }}">{{ $name }}</label>
 </div>
--- resources/views/components/filter-component.blade.php
+++ resources/views/components/filter-component.blade.php
@@ -3,9 +3,9 @@
 <div>
     @if (!$admin)
         <div class="bg-[#B7E7EB] flex rounded-xl">
-            <x-filter-card name="caca"/>
-            <x-filter-card name="pipi" />
-            <x-filter-card name="crampté" last="true"/>
+            <x-filter-card name="teste" first="true" />
+            <x-filter-card name="teste01" />
+            <x-filter-card name="teste02" last="true" />
         </div>
     @else
 
