5fc980c8e104fb7678790662b0c12dfee8780b7c
Drake <150128459+VialsShiny@users.noreply.github.com>
2025-06-23 10:39:22 +0200

    Changing API Route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\AllergeneController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/allergenes', [HomeController::class, 'index']);
+Route::get('/allergenes', [AllergeneController::class, 'all'])->name('allergenes');
