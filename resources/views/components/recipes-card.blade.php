4fed3431979fb23cadf878c7e2de14288830d2d3
a442147 <a442147@edenschool.fr>
2025-06-24 10:26:19 +0200

    Fused code from Natouille and Vials

--- resources/views/components/recipes-card.blade.php
+++ resources/views/components/recipes-card.blade.php
@@ -1,45 +1,37 @@
 @props(['recette'])
 
 <div class="relative">
-    <a href="#">
-        <div class="h-36 my-5 pl-4 flex flex-row justify-between bg-white rounded-xl shadow-sm shadow-gray-500">
-            <div class="flex flex-col justify-around h-full">
-                <h3 class="text-md font-bold py-2">{{$recette->name}}</h3>
-                <p class="text-sm italic">{{$recette->user_name}}</p>
-                <div class="h-18 flex flex-col justify-around text-sm">
-                    <p class="underline">{{$recette->origin_name}}</p>
-                    <p class="font-semibold ">{{$recette->regimes}}</p>
+    <a href="{{ route('recette.show', $recette->references) }}" class="block w-full">
+        <div class="w-full h-36 my-5 flex bg-white rounded-xl shadow-md overflow-hidden">
+
+            {{-- Partie texte --}}
+            <div class="flex flex-col justify-around w-6/12 h-full pl-3 pr-2 z-10 bg-white">
+                <div class="flex flex-col">
+                    <h3 class="text-base font-semibold">{{ $recette->name }}</h3>
+                    <p class="text-sm text-gray-700 italic">{{ $recette->user_name }}</p>
+                </div>
+
+                <div class="flex flex-col">
+                    <p class="text-sm text-gray-600 underline">{{ $recette->origin_name }}</p>
+                    <p class="text-xs text-gray-500 font-semibold">{{ $recette->regimes }}</p>
                 </div>
             </div>
 
-            <div class="flex flex-row items-end w-7/12 h-full bg-amber-200 rounded-r-xl">
-                <div class="absolute h-36 w-1/5 bg-gradient bg-gradient-to-r from-white to-transparent">
+            <div class="flex flex-col justify-between items-end w-6/12 h-full rounded-r-xl p-2 text-yellow-400" style="background-image:
+                                    linear-gradient(to right, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0)),
+                                    url('{{ $recette->image }}');
+                                    background-size: cover;
+                                    background-position: center;">
+
+
+                <div class="bg-[#0E2F46] w-8 h-8 rounded text flex justify-center items-center text-xl text-white">
+                    <i class="ri-bookmark-line"></i>
                 </div>
 
-                <div class="absolute flex my-3 ml-[12%] justify-center text-[1.7ej] leading-none">
-                    <div class="relative flex space-x-1 text-yellow-400">
-                        @for ($i = 0; $i < 5; $i++)
-                            <i class="block ri-star-line text-xs md:text-sm lg:text-md"></i>
-                        @endfor
+                <x-star-counter :rate="$recette->rate"></x-star-counter>
 
-                        <div class="absolute top-0 left-0 flex space-x-1 text-yellow-400 overflow-hidden"
-                            style="width: calc({{ $recette->rate }} * 20%)">
-                            @for ($i = 0; $i < 5; $i++)
-                                <i class="block ri-star-fill text-xs md:text-sm lg:text-md"></i>
-                            @endfor
-                        </div>
-                    </div>
-
-                </div>
-
-                <div
-                    class="absolute rounded-lg flex text-xl lg:text-2xl justify-center items-center top-3 right-3 h-7 w-7 lg:h-8 lg:w-8 bg-[#0E2C46]">
-                    <i class="ri-bookmark-line text-white"></i>
-                </div>
-
-                <img class="w-full h-36 rounded-r-2xl object-cover" src="/images/rice-fried-pork.png"
-                    alt="{{ $recette->name  }}">
             </div>
+
         </div>
     </a>
 </div>
--- resources/views/components/star-counter.blade.php
+++ resources/views/components/star-counter.blade.php
@@ -6,16 +6,16 @@
     $emptyStars = 5 - $fullStars - ($halfStar ? 1 : 0);
 @endphp
 
-<div class="flex space-x-1 text-lg">
+<div class="flex space-x-1 text-[clamp(1rem,2.5vw,1.6rem)] overflow-visible">
     @for ($i = 0; $i < $fullStars; $i++)
-        <i class="ri-star-fill"></i>
+        <i class="ri-star-fill text-shadow-lg/30"></i>
     @endfor
 
     @if ($halfStar)
-        <i class="ri-star-half-line"></i>
+        <i class="ri-star-half-line text-shadow-lg/30"></i>
     @endif
 
     @for ($i = 0; $i < $emptyStars; $i++)
-        <i class="ri-star-line"></i>
+        <i class="ri-star-line text-shadow-lg/10"></i>
     @endfor
 </div>
