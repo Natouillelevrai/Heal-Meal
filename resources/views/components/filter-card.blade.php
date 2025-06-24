8d3f65c6c562e071b851e067198d871656412867
VialsShiny <vialsvialatou@gmail.com>
2025-06-24 14:22:37 +0200

    Patching Category CSS

--- resources/views/components/filter-card.blade.php
+++ resources/views/components/filter-card.blade.php
@@ -1,7 +1,22 @@
-@props(['name' => "default", 'first' => null, 'last' => null])
+@props(['name' => "default", 'index' => 0, 'length' => 0])
 
-<div class="flex">
+@php
+    $translateX = floor($index * 30);
+@endphp
+
+<style>
+    @media (min-width: 768px) {
+        .card-filter-{{ $index }} {
+            transform: translateX(-{{ $translateX }}px);
+        }
+    }
+</style>
+
+<div class="flex card-filter-{{ $index }}">
     <input type="radio" name="filter" id="{{ $name }}" class="hidden peer">
-    <label for="{{ $name }}"
-        class="flex justify-center items-center capitalize max-h-12 min-w-12 px-4 py-2 bg-white {{ !$last ? 'rounded-l-xl' : 'rounded-xl'}} border-3 peer-checked:bg-[#B7E7EB] peer-checked:text-white border-[#B7E7EB] {{ !$first ? 'peer-checked:border-white' : '' }}">{{ $name }}</label>   
+    <label for="{{ $name }}" class="flex justify-center items-center capitalize max-h-12 min-w-12 px-4 py-2 
+        {{ $index == $length - 1 ? '' : 'pr-8' }} 
+        bg-white rounded-xl border-3 peer-checked:bg-[#B7E7EB] peer-checked:text-white border-[#B7E7EB]">
+        {{ $name }}
+    </label>
 </div>
--- resources/views/components/filter-component.blade.php
+++ resources/views/components/filter-component.blade.php
@@ -1,16 +1,22 @@
 @props(['admin' => null])
 
+@php
+    $tab = !$admin ? ['teste01', 'teste02', 'teste03'] : ['admin01', 'admin02', 'admin03'];
+@endphp
+
 <div class="w-full flex items-center justify-between gap-x-12">
     @if (!$admin)
-        <div class="bg-[#B7E7EB] flex flex-wrap rounded-xl">
-            <x-filter-card name="teste" first="true" />
-            <x-filter-card name="teste01" />
-            <x-filter-card name="teste02" last="true" />
+        <div class="flex flex-wrap justify-between md:justify-start gap-y-2 md:gap-y-0 rounded-xl">
+            @foreach ($tab as $key => $filterCard)
+                <x-filter-card :name="$filterCard" :index="$key" :length="count($tab)" />
+            @endforeach
         </div>
     @else
-
+        {{-- No filter cards shown in admin view --}}
     @endif
 
-    <button role="option"
-        class="flex justify-center items-center capitalize max-h-12 min-w-12 px-4 py-2 bg-[#0E2F46] rounded-xl border-3 border-[#0E2F46] text-white">Filter</button>
+    <button
+        class="flex justify-center items-start capitalize max-h-12 min-w-12 px-4 py-2 bg-[#0E2F46] rounded-xl border-3 border-[#0E2F46] text-white">
+        Filter
+    </button>
 </div>
