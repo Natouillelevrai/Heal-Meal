081dae2e399d8ccd82a7b049051354e042236356
VialsShiny <vialsvialatou@gmail.com>
2025-06-26 10:43:03 +0200

    Patching Admin Middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 
-use App\Http\Controllers\Api\AdminController;
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RecipesController;
